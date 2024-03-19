2942e3eb5341ad1182891eb730c27a189c56a9a5
Matthew <matthew.parkes123@googlemail.com>
2024-03-19 22:08:47 +0000

    Made the game functional

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ const pathCharacter = '*';
 class field {
     constructor(fieldValues) {
         this.field = fieldValues;
-    
+        this.currentRow = 0;
+        this.currentValue = fieldValues[0].indexOf('*');
+        this.width = this.field[0].length;
+        this.height = this.field.length;
+        
       }
 
     print(){
@@ -16,6 +20,97 @@ class field {
             console.log(this.field[i].join(''))
         }
        
+    }
+
+    move(e){
+        if(e === 'r'){
+            if(this.currentValue < this.width - 1){
+                this.currentValue += 1
+                this.AddStar();
+                
+            } else {
+                console.log('Out of Bounds - Try Again')
+                this.print()
+            }
+           
+            
+
+        } else if (e === 'd'){
+
+            if(this.currentRow <= (this.height -=1)){
+
+                this.currentRow += 1
+                this.AddStar();
+            } else {
+                console.log('Out of Bounds - Try Again')
+                this.print()
+            }
+            
+            
+
+
+        } else if (e === 'l'){
+
+            if(this.currentValue > 0){
+                this.currentValue -= 1
+                this.AddStar();
+            } else {
+                console.log('Out of Bounds - Try Again')
+                this.print()
+            }
+
+            
+        }
+
+        StopGate = false
+    }
+
+    AddStar(){
+        if(this.field[this.currentRow][this.currentValue] === 'O' ){
+
+            console.log('You Fell Down a Hole! You Lose!')
+            GameOverStatus = true
+        }else if (this.field[this.currentRow][this.currentValue] === '^'){
+
+            console.log('You Found the Hat. You Win!')
+            GameOverStatus = true
+
+        }else {
+            this.field[this.currentRow][this.currentValue] = '*'
+            this.print()
+        }
+        
+    }
+
+    static generateField(height, width, percentage){
+        let result = [];
+        let highestArray = height*width;
+        let total_holes = (height * width) * percentage
+
+        for(let i=0; i < height; i++){
+            let arrayToPush = []
+            for(let j=0; j < width; j++){
+                arrayToPush.push('░')
+
+            }
+            result.push(arrayToPush)
+        }
+
+        // Calculate Position of Hat
+
+        let HatIndex = Math.random() * highestArray
+
+        // Calculate Postion of Holes
+
+        for(let i= 0; i < total_holes; i++){
+            let HoleIndex = Math.random() * total_holes
+            
+
+        }
+    
+
+
+
     }
     
 }
@@ -30,12 +125,14 @@ let newfield = new field([
 console.log(newfield.print())
 
 let StopGate = false
+let GameOverStatus = false 
 
-while(StopGate != true) {
+while(StopGate != true && GameOverStatus === false) {
 
 const NextStep = prompt('What is your next step? Type r for right, l for left, and d for down')
 
 if(NextStep != ('r') && NextStep != ('l') && NextStep != ('d')){
+
 
     console.log('That is not a valid input - try again')
 
@@ -43,6 +140,8 @@ if(NextStep != ('r') && NextStep != ('l') && NextStep != ('d')){
 
     console.log('You selected '+ NextStep)
     StopGate = true
+    newfield.move(NextStep);
+    
 }
 
 }
